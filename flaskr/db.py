1b5edb0ab1d25816b4c294eb90d2c5cb2a728f93
JuliaHT <julia.hamblin@code.berlin>
2019-04-25 21:31:46 +0200

    Connected database & javascript

--- flaskr/db.py
+++ flaskr/db.py
@@ -1,26 +1,22 @@
-import sqlite3
-import click
+import sqlite3 as sql
+from flask import current_app, g
+from flask.cli import with_appcontext
 
-
-
-#conn = sqlite3.connect('database.db')
-#conn.execute('CREATE TABLE users (name TEXT, email TEXT, password TEXT)')
-#conn.close()
 def get_db(commend):
-    conn = sqlite3.connect('groot_tasks.db')
+    conn = sql.connect('groot_tasks.db')
     cur = conn.cursor()
     cur.execute(commend)
     rows = cur.fetchall()  
-    print(rows)   
-    return rows 
+    return rows
 
 
 
 
 
 def insert_db(commend):
-    conn = sqlite3.connect('groot_tasks.db')
+    conn = sql.connect('groot_tasks.db')
     cur = conn.cursor()
     cur.execute(commend)
     conn.commit()
     conn.close()
+
--- flaskr/groot.py
+++ flaskr/groot.py
@@ -1,11 +1,54 @@
 from flask import Flask, render_template, session, request, redirect, flash, url_for
 app = Flask(__name__)
 import sqlite3 as sql
+import db
 
 @app.route("/")
 def groot():
-    return render_template("index.html", name="groot")
-    # con = sql.connect("groot_tasks.db")
+    # get info from challange table in database
+    challenge_overview = db.get_db("""
+    SELECT challenges.Title, levels.Name, challenges.Description, challenges.ID
+    FROM challenges
+    INNER JOIN levels ON challenges.Level=levels.Level;
+    """)
+    # make div clickable and link to new page
+
+    return render_template("index.html", challenge_overview = challenge_overview)
+
+@app.route("/login", methods=("GET", "POST"))
+def login():
+    if request.method == "GET":
+        return render_template("login.html")        
+    else:
+        username_entered = request.form["username"]
+        password_entered = request.form["password"]
+
+        if not username_entered or not password_entered:
+            return "Please input your data."
+        
+        user_info = db.get_db("SELECT username, password FROM user WHERE username = '{}' ".format(username_entered))
+
+        # check if user_info is there
+        if user_info:
+            # compare username_entered to username in database
+            if username_entered == user_info[0][0]:
+                # compare password_entered to password in database
+                if password_entered == user_info[0][-1]:
+                    # return homepage
+                    return redirect("/")           
+                # if password is inaccurate return login page
+                else: 
+                    return("The password is incorrect. Please try again.")
+            # if username is inaccurate return login page
+            else:
+                return("The username is incorrect. Please try again.")
+        else:
+            return("Wrong login information.")
+
+        print(user_info)
+        return "working"
+
+      
 
 if __name__ == "__main__":
     app.run()
